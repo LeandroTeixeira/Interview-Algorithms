e6b502a33c1ffe5779b988dc9548feecd7068557
LeandroTeixeira <leandroteixeira3@gmail.com>
2021-01-06 11:43:14 -0300

    Update solution.py

--- Best Time to Buy and Sell Stock/solution.py
+++ Best Time to Buy and Sell Stock/solution.py
@@ -1,19 +1,18 @@
-import math
-
-def solution (test):
-    '''
+class Solution:
+    def maxProfit (self,test):
+        '''
     Description:
-        This function iterates through the array comparing the current value with the lowest found so far and see if it makes a profit higher than the best one found so far. Infinity and minus infinity are used as the starting lowest price and max profit, respectively\n
+        This function iterates through the array comparing the current value with the lowest found so far and see if it makes a profit higher than the best one found so far. Infinity and zero are used as the starting lowest price and max profit, respectively\n
         Note that the highest profit returned can be negative if the price goes only down\n
     Complexity: O(n)
         We iterate through the array only once
     '''
-    lowest_price = math.inf
-    max_profit = -math.inf
-    for i in test:
-        if (n:= i - lowest_price) > max_profit:
-            max_profit = n
-        if i < lowest_price:
-            lowest_price = i 
-    return max_profit 
+        lowest_price = test[0]
+        max_profit = 0
+        for i in test[1:]:
+            if (n:= i - lowest_price) > max_profit:
+                max_profit = n
+            if i < lowest_price:
+                lowest_price = i 
+        return max_profit 
 
